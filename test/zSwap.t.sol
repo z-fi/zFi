4f0c257d3496adc0a8173f62b2c869e82f20908a
z0r0z <zrzr8963@gmail.com>
2026-09-23 00:07:06 +0700

    zSwap: touch-sized farm, airdrop and creator-fee links on phones
    Links in the small status lines under the swap card get extra padding on
    coarse pointers, so each is at least 30px square. Page hash, chunk
    calldata and payload sizes regenerated.

--- test/zSwap.t.sol
+++ test/zSwap.t.sol
@@ -7,8 +7,8 @@ import {zSwap} from "../src/zSwap.sol";
 contract zSwapDeployTest is Test {
     // keccak256 and length of zSwap.html. To recompute after editing the dapp:
     //   node -e "const e=require('ethers'),fs=require('fs');const h=fs.readFileSync('zSwap.html');console.log(e.keccak256(h),h.length)"
-    bytes32 constant EXPECTED_HASH = 0xef9308dcddab511b6dda45b77ddcc2ca292a78cc7f2780929fdb26a8fb61a785;
-    uint256 constant EXPECTED_LEN = 630640;
+    bytes32 constant EXPECTED_HASH = 0x6e2ac0619a813feefbc63e82482e2f2b93809db4eb50955445076d13e9bdf30e;
+    uint256 constant EXPECTED_LEN = 630727;
 
     /// @dev Deploys `data` as a contract whose runtime bytecode IS that data,
     /// mirroring how the chunks are deployed on-chain (PUSH2 len, DUP1,
