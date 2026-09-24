aab57762805668ac1029bb216d40f2c12270131a
z0r0z <zrzr8963@gmail.com>
2026-09-25 00:29:18 +0700

    zSwap: the pins carry the page with the solver executor
    666,163 B across 28 chunks, 21,965 B of headroom.

--- test/zSwap.t.sol
+++ test/zSwap.t.sol
@@ -7,8 +7,8 @@ import {zSwap} from "../src/zSwap.sol";
 contract zSwapDeployTest is Test {
     // keccak256 and length of zSwap.html. To recompute after editing the dapp:
     //   node -e "const e=require('ethers'),fs=require('fs');const h=fs.readFileSync('zSwap.html');console.log(e.keccak256(h),h.length)"
-    bytes32 constant EXPECTED_HASH = 0xac831ecb87d90442968c5f5efadb2ce63c50014404e896d9ccb0da09fdfd96bf;
-    uint256 constant EXPECTED_LEN = 666019;
+    bytes32 constant EXPECTED_HASH = 0xcf6e15ebb997e91dd8618ce1de7a88a3efb9af2cc96564e0b51351aa22c571ca;
+    uint256 constant EXPECTED_LEN = 666163;
 
     /// @dev Deploys `data` as a contract whose runtime bytecode IS that data,
     /// mirroring how the chunks are deployed on-chain (PUSH2 len, DUP1,
